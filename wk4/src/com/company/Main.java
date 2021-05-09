228135e759e1e51f43885cfb018a43eb6e5f6b9f
xeviousgreen <xeviousgreen@gmail.com>
2021-05-08 21:09:29 -0600

    lets go

---
+++ wk4/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main {
+
+//http://www.json-generator.com/api/json/get/bPuptCNIuq?indent=2
+    //
+    public static void main(String[] args) throws MalformedURLException {
+        URL url = new URL("http://www.json-generator.com/api/json/get/bPuptCNIuq?indent=2");
+
+
+            }
+}
