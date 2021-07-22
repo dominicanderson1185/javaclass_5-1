2ac12a2d491c4aea6565da423090042b23db9d3a
xeviousgreen <xeviousgreen@gmail.com>
2021-07-21 21:46:30 -0600

    update

--- login_hibernate/src/main/java/Hibermodel/hi_mod_ext.java
+++ login_hibernate/src/main/java/Hibermodel/hi_mod_ext.java
@@ -5,15 +5,15 @@ public class hi_mod_ext {
 	private String password;
 
 
-public void Main(String username, String password) {
+public hi_mod_ext(String username, String password) {
 this.password = password;
 this.username = username;
 }
-	public String getPassword() {
+	public String yoPassword() {
 	return password;
 	}
 
-	public String getUsername() {
+	public String yoUsername() {
 		return username;
 	};
 }
--- login_hibernate/src/test/java/Hibermodel/hi_mod_extTest.java
+++ login_hibernate/src/test/java/Hibermodel/hi_mod_extTest.java
@@ -13,9 +13,24 @@ class hi_mod_extTest {
 
 	@Test
 	void getPassword() {
+
+		hi_mod_ext x = new hi_mod_ext("student","lost");
+		assertEquals("lost", x.yoPassword());
 	}
 
 	@Test
 	void getUsername() {
+		hi_mod_ext x = new hi_mod_ext("student@yahoo","lost");
+		assertEquals("student@yahoo", x.yoUsername() );
 	}
-}
+	@Test
+	void getUsername1() {
+		hi_mod_ext x = new hi_mod_ext("student","lost");
+		assertEquals("student", x.yoPassword());}
+
+		@Test
+		void getPassword2(){
+			hi_mod_ext x = new hi_mod_ext("student","lost312");
+			assertEquals("lost312", x.yoPassword());
+		}
+	}
